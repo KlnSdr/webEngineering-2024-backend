00da78203f5894714a2ecf443c71da19aa6502aa
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-07-05 22:41:26 +0200

    add demoService

---
+++ Demo/service/src/main/java/com/sks/demo/service/Listener.java
@@ -0,0 +1,20 @@
+package com.sks.demo.service;
+
+import com.sks.demo.api.*;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Listener implements DemoListener {
+    private final DemoSender sender;
+
+    public Listener(DemoSender sender) {
+        this.sender = sender;
+    }
+
+    @Override
+    public void listen(DemoRequestMessage baseMessage) {
+        System.out.println(baseMessage.getMessage());
+        // ...
+        sender.sendResponse(baseMessage, new DemoResponseMessage("Listener got message: " + baseMessage.getMessage()));
+    }
+}
