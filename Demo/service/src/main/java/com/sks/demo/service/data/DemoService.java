66f381766dd4f34125650c4e4fb89e46abfb5dc7
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-07-08 23:31:09 +0200

    [BACKEND-10]: add connection to mariadb

---
+++ Demo/service/src/main/java/com/sks/demo/service/data/DemoService.java
@@ -0,0 +1,18 @@
+package com.sks.demo.service.data;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DemoService {
+    private DemoRepository demoRepository;
+
+    public List<DemoEntity> getAll() {
+        return demoRepository.findAll();
+    }
+
+    public DemoEntity save(DemoEntity demoEntity) {
+        return demoRepository.save(demoEntity);
+    }
+}
