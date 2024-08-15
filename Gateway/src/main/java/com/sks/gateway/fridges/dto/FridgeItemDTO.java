82d943b6a74c54de57d94246865bf17fe7c4ef7f
rkirche2 <ronja.kircheis@stud.htwk-leipzig.de>
2024-08-15 18:55:32 +0200

    [BACKEND-19]: DTO class for fridge items

--- Gateway/src/main/java/com/sks/gateway/fridges/dto/FridgeItemDTO.java
+++ Gateway/src/main/java/com/sks/gateway/fridges/dto/FridgeItemDTO.java
@@ -2,13 +2,14 @@ package com.sks.gateway.fridges.dto;
 
 public class FridgeItemDTO {
 
-    public class FridgeItemDTO {
         private String name;
-        private String id;
+        private long id;
         private String unit;
         private double quantity;
 
-        public FridgeItemDTO(String name, String id, String unit, double quantity) {
+        public FridgeItemDTO(){}
+
+        public FridgeItemDTO(String name, long id, String unit, double quantity) {
             this.name = name;
             this.id = id;
             this.unit = unit;
@@ -16,17 +17,36 @@ public class FridgeItemDTO {
         }
 
         // Getters and Setters
-        public String getName() { return name; }
-        public void setName(String name) { this.name = name; }
+        public String getName() {
+                return name;
+        }
 
-        public String getId() { return id; }
-        public void setId(String id) { this.id = id; }
+        public void setName(String name) {
+                this.name = name;
+        }
 
-        public String getUnit() { return unit; }
-        public void setUnit(String unit) { this.unit = unit; }
+        public long getId() {
+                return id;
+        }
 
-        public double getQuantity() { return quantity; }
-        public void setQuantity(double quantity) { this.quantity = quantity; }
-    }
+        public void setId(long id) {
+                this.id = id;
+        }
+
+        public String getUnit() {
+                return unit;
+        }
+
+        public void setUnit(String unit) {
+                this.unit = unit;
+        }
+
+        public double getQuantity() {
+                return quantity;
+        }
+
+        public void setQuantity(double quantity) {
+                this.quantity = quantity;
+        }
 
 }
