dd62b267c8ae4b8e8486dd6b1b1a78a6915c655a
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-01 16:09:04 +0200

    [BACKEND-35]: add swagger docs to rest resources

--- Gateway/webapp/src/main/java/com/sks/gateway/products/rest/ProductsResource.java
+++ Gateway/webapp/src/main/java/com/sks/gateway/products/rest/ProductsResource.java
@@ -4,6 +4,13 @@ import com.sks.products.api.ProductDTO;
 import com.sks.products.api.ProductsRequestMessage;
 import com.sks.products.api.ProductsResponseMessage;
 import com.sks.products.api.ProductsSender;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -18,6 +25,18 @@ public class ProductsResource {
         this.productsSender = productsSender;
     }
 
+    @Operation(summary = "Get all products")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found all products",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ProductDTO.class))
+                            )
+                    })
+    })
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public ProductDTO[] getAllProducts() {
@@ -25,9 +44,23 @@ public class ProductsResource {
         return response.getProducts();
     }
 
+    @Operation(summary = "Get product by ID")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the product",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = ProductDTO.class)
+                            )
+                    }),
+            @ApiResponse(responseCode = "404", description = "Product not found", content = @Content)
+    })
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ProductDTO getProductById(@PathVariable("id") long id) {
+    public ProductDTO getProductById(
+            @Parameter(description = "ID of the product to be fetched") @PathVariable("id") long id) {
         final ProductsResponseMessage response = productsSender.sendRequest(new ProductsRequestMessage(new long[] {id}));
         final ProductDTO[] product = response.getProducts();
 
@@ -38,9 +71,23 @@ public class ProductsResource {
         return product[0];
     }
 
+    @Operation(summary = "Get multiple products by IDs")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Found the products",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = ProductDTO.class))
+                            )
+                    }),
+            @ApiResponse(responseCode = "404", description = "Products not found", content = @Content)
+    })
     @PostMapping(value = "get-multiple", produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public ProductDTO[] getMultiple(@RequestBody long[] ids) {
+    public ProductDTO[] getMultiple(
+            @Parameter(description = "IDs of the products to be fetched") @RequestBody long[] ids) {
         final ProductsResponseMessage response = productsSender.sendRequest(new ProductsRequestMessage(ids));
         final ProductDTO[] products = response.getProducts();
 
