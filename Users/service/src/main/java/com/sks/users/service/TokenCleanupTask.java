f90e5083ac4c71eee40656f76c4f48b804ba1b58
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-07 13:27:59 +0200

    [BACKEND-37]: periodically remove expired tokens

---
+++ Users/service/src/main/java/com/sks/users/service/TokenCleanupTask.java
@@ -0,0 +1,25 @@
+package com.sks.users.service;
+
+import com.sks.users.service.data.TokenService;
+import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TokenCleanupTask {
+    private static final Logger log = LoggerFactory.getLogger(TokenCleanupTask.class);
+    private final TokenService tokenService;
+
+    public TokenCleanupTask(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    @Scheduled(fixedDelay = 5000)
+    @Transactional
+    public void cleanup() {
+        log.info("Cleaning up expired tokens");
+        tokenService.deleteExpired();
+    }
+}
--- Users/service/src/main/java/com/sks/users/service/data/TokenRepository.java
+++ Users/service/src/main/java/com/sks/users/service/data/TokenRepository.java
@@ -2,5 +2,8 @@ package com.sks.users.service.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+
 public interface TokenRepository extends JpaRepository<TokenEntity, String> {
+    void deleteByValidTillBefore(Date now);
 }
