970a015a8478b45ebccb80334c2631daff3ef7a2
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-07 15:28:39 +0200

    [BACKEND-37]: set token cleanup interval in properties

--- Users/service/src/main/java/com/sks/users/service/TokenCleanupTask.java
+++ Users/service/src/main/java/com/sks/users/service/TokenCleanupTask.java
@@ -4,6 +4,7 @@ import com.sks.users.service.data.TokenService;
 import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -11,15 +12,21 @@ import org.springframework.stereotype.Component;
 public class TokenCleanupTask {
     private static final Logger log = LoggerFactory.getLogger(TokenCleanupTask.class);
     private final TokenService tokenService;
+    @Value("${service.tokens.cleanup.interval}")
+    private long fixedDelay;
 
     public TokenCleanupTask(TokenService tokenService) {
         this.tokenService = tokenService;
     }
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelayString = "#{@tokenCleanupTask.fixedDelay}")
     @Transactional
     public void cleanup() {
         log.info("Cleaning up expired tokens");
         tokenService.deleteExpired();
+    }
+
+    public long getFixedDelay() {
+        return fixedDelay;
     }
 }
