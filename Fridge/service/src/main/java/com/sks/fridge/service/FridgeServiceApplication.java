53a6c5d27f800f7b532791afc97e499355c826ef
Kilian Schneider <kilian.schneider@stud.htwk-leipzig.de>
2024-09-08 23:06:44 +0200

    remove auto insert classes

--- Fridge/service/src/main/java/com/sks/fridge/service/FridgeServiceApplication.java
+++ Fridge/service/src/main/java/com/sks/fridge/service/FridgeServiceApplication.java
@@ -1,6 +1,5 @@
 package com.sks.fridge.service;
 
-import com.sks.fridge.service.data.entity.AutoInsert;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,10 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.sks")
 public class FridgeServiceApplication {
-
-	public FridgeServiceApplication(AutoInsert autoInsert) {
-		autoInsert.run();
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(FridgeServiceApplication.class, args);
--- Recipes/service/src/main/java/com/sks/recipes/service/RecipesServiceApplication.java
+++ Recipes/service/src/main/java/com/sks/recipes/service/RecipesServiceApplication.java
@@ -1,8 +1,5 @@
 package com.sks.recipes.service;
 
-import com.sks.recipes.service.data.entity.AutoInsert;
-import jakarta.persistence.PersistenceUnit;
-import org.hibernate.SessionFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,11 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan("com.sks")
 public class RecipesServiceApplication {
-
-	public RecipesServiceApplication(AutoInsert autoInsert) {
-		autoInsert.run();
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesServiceApplication.class, args);
 	}
